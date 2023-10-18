ee80162b0fa0e2e3a123158b56faa173dc45a76a
HemaSriGaniJ9 <119720904+HemaSriGaniJ9@users.noreply.github.com>
2023-10-18 21:23:05 +0530

    a = int(input()) if a>1:     for i in range(2,int(a/2)+1):         if(a%i)==0:             print("not a prime")             break     else:             print("prime") else:     print("not a prime")

---
+++ prime_number.py
@@ -0,0 +1,10 @@
+a = int(input())
+if a>1:
+    for i in range(2,int(a/2)+1):
+        if(a%i)==0:
+            print("not a prime")
+            break
+    else:
+            print("prime")
+else:
+    print("not a prime")
