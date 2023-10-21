b539b2bb6c6a72d3db811838136a117882d1c89c
HemaSriGaniJ9 <119720904+HemaSriGaniJ9@users.noreply.github.com>
2023-10-21 23:52:18 +0530

    a = int(input()) sq=a*a s=0 q=sq while q>0:     r=q%10     s=s+r     q=q//10 if(a==s):     print("Neon Number") else:     print("Not Neon Number")

--- Neon_Number.py
+++ Neon_Number.py
@@ -1,12 +1,12 @@
 a = int(input())
+sq=a*a
 s=0
-b=a*a
-while b>0:
-    d=b%10
-    s=s+d
-    b=b//10
-if(s==a):
+q=sq
+while q>0:
+    r=q%10
+    s=s+r
+    q=q//10
+if(a==s):
     print("Neon Number")
 else:
     print("Not Neon Number")
-
