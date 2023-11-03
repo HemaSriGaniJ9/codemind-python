95169156c3a5ebd49b9464d5b71f5a1d9fdbb590
HemaSriGaniJ9 <119720904+HemaSriGaniJ9@users.noreply.github.com>
2023-11-03 15:03:06 +0530

    r,c = map(int,input().split()) mat=[] for i in range(r):     inner_list = list(map(int,input().split()))[:c:]     mat.append(inner_list) s=0 for inner_list in mat:     for every_list in inner_list:         s+= every_list print(s)

---
+++ Sum_of_all_elements_in_a_matrix.py
@@ -0,0 +1,11 @@
+r,c = map(int,input().split())
+mat=[]
+for i in range(r):
+    inner_list = list(map(int,input().split()))[:c:]
+    mat.append(inner_list)
+s=0
+for inner_list in mat:
+    for every_list in inner_list:
+        s+= every_list
+print(s)
+    
